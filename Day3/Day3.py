399683e35782cde286e731bc5c1b724d75107403
Zachary Boerner <Zachary.Boerner@gmail.com>
2015-12-20 15:37:10 -0700

    Day 3's solution and input.

---
+++ Day3/Day3.py
@@ -0,0 +1,135 @@
+from operator import mul
+from functools import reduce
+
+totalSquareFootage = 0
+totalRibbon = 0
+
+with open('Day3Input.txt', 'r') as file:
+	for line in file:
+		line = line.strip()
+
+		xMostNegative = 0
+		yMostNegative = 0
+
+		movesLeft  = 0
+		movesRight = 0
+		movesUp    = 0
+		movesDown  = 0
+
+		santaPos = [(0, 0)]
+
+		curXPos = 0
+		curYPos = 0
+		xPosSanta = [0]
+		yPosSanta = [0]
+
+		for character in line:
+			if character is '<':
+				xPosSanta.append(xPosSanta[-1] + 1)
+				curXPos += 1
+			elif character is '>':
+				xPosSanta.append(xPosSanta[-1] - 1)
+				curXPos -= 1
+			elif character is '^':
+				yPosSanta.append(yPosSanta[-1] + 1)
+				curYPos += 1
+			elif character is 'v':
+				yPosSanta.append(yPosSanta[-1] - 1)
+				curYPos	-= 1
+			santaPos.append((curXPos, curYPos))
+
+		# While we functionally just did this above, it's easier to read redoing this calculation:
+		numUp    = line.count('^')
+		numDown  = -1 * line.count('v')
+		numRight = line.count('>')
+		numLeft  = -1 * line.count('<')
+
+		# Grid I used for figuring out the next calculation
+		# There will always be a dimension of 1 - Santa starts on the first square
+		# But, the bounds don't just scale based on how many he goes right or left.  After all, Santa could just cycle
+		# between two houses.  It's the difference between the two that dictates how it scales
+		#
+		# >>^^vvvv gives:
+		#
+		#     x x 5
+		#     x x 6
+		#     1 2 7
+		#     x x 8
+		#     x x 9
+
+		horizDimen = 1 + max(xPosSanta) - min(xPosSanta)
+		vertDimen  = 1 + max(yPosSanta) - min(yPosSanta)
+		print("Horizontal dimension: %s (Min: %s, Max: %s)" % (horizDimen, min(xPosSanta), max(xPosSanta)))
+		print("Vertical dimension: %s (Min: %s, Max: %s)" % (vertDimen, min(yPosSanta), max(yPosSanta)))
+
+		uniqueHouses = list(set(santaPos))
+		print("Unique houses visited: %s" % len(uniqueHouses))
+
+
+
+		# Everything below here is garbage, but I'm leaving it for posterity's sake
+
+		# houseGrid = [0] * horizDimen
+		# for i in range(horizDimen):
+		# 	houseGrid[i] = [0] * vertDimen
+
+		# # Now for his position.  He will start at 0, 0 (in the case of not moving)
+		# # For every movement that extends the bounds to the right and up, it does not affect his starting location
+		# # Example:
+		# #
+		# # 	  >>^^ produces:
+		# #
+		# #         x x 4
+		# #         x x 3
+		# #         S 1 2
+		# #
+		# #     Santa still starts at 0, 0.
+		# #
+		# #     But if he instead does <<vv:
+		# #
+		# #         2 1 S
+		# #         3 x x
+		# #         4 x x
+		# #
+		# #     Santa now starts at 2, 2 (indexing at 0).
+		# #
+		# # This means that the only thing affecting his starting location are the negative moves that extend the board,
+		# # meaning that we only care about the maximum negative value.
+		# #
+		# # Example:
+		# #
+		# #     ><<vv^:
+		# #
+		# #         3 S 1
+		# #         6 x x
+		# #         5 x x
+		# #
+		# #     Santa starts at 1, 2 (most negative in x was -1 and most negative in y was -2)
+
+		# curXPos = abs(min(xPosSanta))
+		# curYPos = abs(min(yPosSanta))
+
+		# # Don't forget the first house got a present:
+		# houseGrid[curXPos][curYPos] += 1
+
+		# for character in line:
+		# 	if character is '<':
+		# 		curXPos += 1
+		# 	elif character is '>':
+		# 		curXPos -= 1
+		# 	elif character is '^':
+		# 		curYPos += 1
+		# 	elif character is 'v':
+		# 		curYPos -= 1
+
+		# 	houseGrid[curXPos][curYPos] += 1
+
+		# numHousesWithPresents = 0
+
+		# for x in range(horizDimen):
+		# 	for y in range(vertDimen):
+		# 		if houseGrid[x][y] > 0:
+		# 			numHousesWithPresents += 1
+
+		# print("Number of houses with presents: %s" % numHousesWithPresents)
+
