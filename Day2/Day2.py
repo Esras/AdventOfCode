ec80e437386421803ff526c5e8778a95ed74d5f6
Zachary Boerner <Zachary.Boerner@gmail.com>
2015-12-20 14:36:50 -0700

    Added comments and the code for part 2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,15 +1,21 @@
+from operator import mul
+from functools import reduce
 
 totalSquareFootage = 0
+totalRibbon = 0
 
 with open('Day2Input.txt', 'r') as file:
 	for line in file:
 		line = line.strip()
 
+		# Split on the x - we know that each line will be of the form #x#x#
 		listOfNumsStr = line.split('x')
 
+		# Turn each of those values into an integer and sort the list
 		dimensions = [int(i) for i in listOfNumsStr]
+		dimensions.sort()
 
-#		print(dimensions)
+		# print(dimensions)
 
 		aSide = []
 
@@ -17,10 +23,15 @@ with open('Day2Input.txt', 'r') as file:
 		aSide.append(dimensions[0] * dimensions[1])
 		aSide.append(dimensions[0] * dimensions[2])
 		aSide.append(dimensions[1] * dimensions[2])
+		aSide.sort() # Sort for upcoming ribbon
 
 		for side in aSide:
 			totalSquareFootage += 2 * side
 
-		totalSquareFootage += min(aSide)
+		totalSquareFootage += aSide[0] # Only need to add the smallest face for the slack in wrapping paper
 
-print(totalSquareFootage)
+		totalRibbon += 2 * (dimensions[0] + dimensions[1])
+		totalRibbon += reduce(mul, dimensions)
+
+print("Total Wrapping Paper with Extra: %s" % totalSquareFootage)
+print("Total Ribbon with Extra: %s" % totalRibbon)
