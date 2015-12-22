d315df524a9d423ff7d1f1bc22fae31e02f955c5
Zachary Boerner <Zachary.Boerner@gmail.com>
2015-12-21 22:08:29 -0700

    Add code for Day8 part 2.  Did a nifty trick I picked up from the subreddit by replacing each pattern with some character not used in the strings.  Also used r'' as literals to help with the backslash explosion

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -3,6 +3,7 @@ import re as re
 stringCodeCount = 0
 memoryCount = 0
 
+# Part 1
 with open('Day8Input.txt', 'r') as file:
 	for line in file:
 		line = line.strip()
@@ -15,12 +16,12 @@ with open('Day8Input.txt', 'r') as file:
 		line = line[1:-1]
 
 		pattern = r'\\"'
-		line = re.sub(pattern, 'S', line)
-
-		pattern = r'\\\\'
 		line = re.sub(pattern, 'A', line)
 
-		pattern = r'\\[x][0-9a-fA-F][0-9A-Fa-f]'
+		pattern = r'\\\\'
+		line = re.sub(pattern, 'B', line)
+
+		pattern = r'\\[x][0-9a-f][0-9a-f]'
 		line = re.sub(pattern, 'C', line)
 
 		memCountForLine = len(line)
@@ -28,3 +29,40 @@ with open('Day8Input.txt', 'r') as file:
 		memoryCount += memCountForLine
 
 print("Difference: %s" % (stringCodeCount - memoryCount))
+
+stringCodeCount = 0
+newEncodedCount = 0
+
+# Part 2
+with open('Day8Input.txt', 'r') as file:
+	for line in file:
+		line = line.strip()
+
+		stringCodeCount += len(line)
+
+		line = line[1:-1]
+
+		pattern = r'\\\"'
+		line = re.sub(pattern, r'A', line)
+
+		pattern = r'\\\\'
+		line = re.sub(pattern, r'B', line)
+
+		pattern = r'\"'
+		line = re.sub(pattern, r'D', line)
+
+		pattern = r'\\x'
+		line = re.sub(pattern, r'C', line)
+
+
+		line = line.replace('A', r'\\\"')
+		line = line.replace('B', r'\\\\')
+		line = line.replace('C', r'\\x')
+		line = line.replace('D', r'\"')
+
+		line = r'"\"' + line + r'"\"'
+		print("%s: %s" % (line, len(line)))
+
+		newEncodedCount += len(line)
+
+print("Difference: %s" % (newEncodedCount - stringCodeCount))
