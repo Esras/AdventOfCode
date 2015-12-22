d4c10605f78fc786869c14ac5ff7b02dd0852660
Zachary Boerner <Zachary.Boerner@gmail.com>
2015-12-22 00:50:32 -0700

    Day11 Part 1.  This is sloooooow code.  I know there's lots of optimizations I could make, but it's 1 AM.

---
+++ Day11/Day11.py
@@ -0,0 +1,90 @@
+import re
+
+def IncrementString(str):
+
+	lastChar = str[-1]
+	rolloverCount = 0
+
+	while lastChar == 'z':
+		rolloverCount += 1
+		if not rolloverCount == len(str):
+			lastChar = str[-1 - rolloverCount]
+		else:
+			return 'a' * (rolloverCount + 1)
+
+	nextChar = chr(ord(lastChar) + 1)
+
+	if rolloverCount > 0:
+		newString = str[:-1 - rolloverCount] + nextChar + ('a' * rolloverCount)
+	else:
+		newString = str[:-1] + nextChar
+
+	return newString
+
+
+def CheckCharRequirement(str):
+
+	if re.search(r'[iol]', str) is None:
+		return True
+
+	return False
+
+
+def CheckForConsecutive(str):
+	incrementPattern = False
+	location = 0
+	consecutiveCount = 1
+
+	while not incrementPattern:
+		if location + 1 > len(str) - 1: # Location tracks index, length is tracking size
+			return False
+		char = str[location]
+		nextChar = str[location + 1]
+		expChar = chr(ord(char) + 1)
+		#print("char: %s nextChar: %s expChar: %s" % (char, nextChar, expChar))
+		if nextChar == expChar:
+			#print("Incremented consecutiveCount on %s %s" % (nextChar, expChar))
+			consecutiveCount += 1
+		else:
+			consecutiveCount = 1
+		location += 1
+		if consecutiveCount == 3:
+			return True
+
+	return False
+
+
+def CheckForRepeatingChars(str):
+	matches = re.findall(r'([a-z])\1', str)
+	if len(set(matches)) >= 2:
+		return True
+	else:
+		return False
+
+
+def CheckValidity(str):
+
+	if not CheckCharRequirement(str):
+		return False
+	elif not CheckForConsecutive(str):
+		return False
+	elif not CheckForRepeatingChars(str):
+		return False
+	else:
+		return True
+
+
+with open('Day11Input.txt', 'r') as file:
+	for line in file:
+		password = line.strip()
+
+		print("Original Password: %s" % password)
+
+		passwordValid = False
+
+		while not passwordValid:
+			password = IncrementString(password)
+			print(password)
+			passwordValid = CheckValidity(password)
+
+		print("Next Password: %s" % password)
